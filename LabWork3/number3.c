7fa399c5f9cc954c09d833dd940370830c800174
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-03-26 20:37:46 +0300

    Add a solution to task #3

---
+++ LabWork3/number3.c
@@ -0,0 +1,22 @@
+#include "main_header.h"
+#define READ_LENGTH 1000
+
+int main(int argc, char* argv[]) {
+    char buffer[READ_LENGTH];
+    int cp_from, cp_to;
+    cp_from = open(argv[1], O_RDONLY | S_IRUSR, S_IWUSR);
+    if (cp_from == -1) {
+        perror("Open base file error: ");
+        return 1;
+    }
+    cp_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (cp_to == -1) {
+        perror("Open destination file error: ");
+        return 1;
+    }
+    read(cp_from, buffer, sizeof(buffer));
+    write(cp_to, buffer, sizeof(buffer));
+    close(cp_from);
+    close(cp_to);
+    return 0;
+}
