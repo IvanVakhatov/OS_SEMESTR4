58d1e887cc53ac2012dc109a0b5758ff609f4316
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-03-27 23:29:48 +0300

    Fixed crucial bugs

--- LabWork3/number4.c
+++ LabWork3/number4.c
@@ -1,22 +1,31 @@
 #include "main_header.h"
+
 #define READ_LENGTH 1024
+char buffer[READ_LENGTH];
 
 int main(int argc, char* argv[]) {
-    char buffer[READ_LENGTH];
-    //char buffer_backwards[READ_LENGTH];
-    //ssize_t count;
-    int fd = open(argv[1], O_RDONLY);
+    int fd, up = 0, down = 0;
+    char temp;
+
+    fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
         perror("Open base file error: ");
         return 1;
     }
     read(fd, buffer, sizeof(buffer));
     write(STDOUT_FILENO, buffer, sizeof(buffer));
-    /*for (int i = strlen(buffer); i > 0; i--) {
-        printf("%d\n", i);
-        buffer_backwards[i] = buffer[READ_LENGTH - i];
-    }*/
-    //write(STDOUT_FILENO, buffer_backwards, READ_LENGTH);
+    write(STDOUT_FILENO, "\n", 1);
+    
+    down = strlen(buffer) - 1;
+    while (up < down) {
+        temp = buffer[down];  
+        buffer[down] = buffer[up];
+        buffer[up] = temp;  
+        up++;
+        down--; 
+    }
+    write(STDOUT_FILENO, buffer, sizeof(buffer));
+    write(STDOUT_FILENO, "\n", 1);
     close(fd);
     return 0;
 }
