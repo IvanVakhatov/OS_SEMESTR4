8a9a3b33e90c20859e485c20152b076657f0d6b7
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-03-28 10:50:53 +0300

    Added a new functional

--- LabWork3/number1.c
+++ LabWork3/number1.c
@@ -8,6 +8,10 @@ int main() {
     close(fd);
     printf("File descriptor after closing: %d\n", fd);
     fd = open("startup.txt", O_RDONLY);
+    if (fd == -1) {
+        perror("Open file: ");
+        return 1;
+    }
     printf("File descriptor after 'open' method: %d\n", fd);
     return 0;
 }
