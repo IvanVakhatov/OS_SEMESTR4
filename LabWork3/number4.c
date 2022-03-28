296694655dc3b2d5f147be1c215b8930823ddfca
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-03-28 12:03:44 +0300

    Rewrite logic

--- LabWork3/number4.c
+++ LabWork3/number4.c
@@ -1,30 +1,21 @@
 #include "main_header.h"
 
-#define READ_LENGTH 1024
-char buffer[READ_LENGTH];
-
 int main(int argc, char* argv[]) {
-    int fd, up = 0, down = 0;
-    char temp;
+    int fd, file_size;
+    char buffer[1];
 
     fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
         perror("Open base file error: ");
         return 1;
     }
-    read(fd, buffer, sizeof(buffer));
-    write(STDOUT_FILENO, buffer, sizeof(buffer));
-    write(STDOUT_FILENO, "\n", 1);
-    
-    down = strlen(buffer) - 1;
-    while (up < down) {
-        temp = buffer[down];  
-        buffer[down] = buffer[up];
-        buffer[up] = temp;  
-        up++;
-        down--; 
+
+    file_size = lseek(fd, -1, SEEK_END);
+    while (file_size >= 0) {
+        read(fd, buffer, 1);
+        write(STDOUT_FILENO, buffer, 1);
+        file_size = lseek(fd, -2, SEEK_CUR);
     }
-    write(STDOUT_FILENO, buffer, sizeof(buffer));
     write(STDOUT_FILENO, "\n", 1);
     close(fd);
     return 0;
