38f9828d130c6da4ed0f94e70d4641d1712e7f5f
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-03-31 11:37:27 +0300

    Update a logic

--- LabWork3/number4.c
+++ LabWork3/number4.c
@@ -2,7 +2,12 @@
 
 int main(int argc, char* argv[]) {
     int fd, file_size;
-    char buffer[1];
+    char buffer;
+    /* int a = 123123;
+    void* num = &a;
+    printf("%d\n",*((int*)(num))); */
+    
+    //*(([тип])([имя]))
 
     fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
@@ -12,8 +17,8 @@ int main(int argc, char* argv[]) {
 
     file_size = lseek(fd, -1, SEEK_END);
     while (file_size >= 0) {
-        read(fd, buffer, 1);
-        write(STDOUT_FILENO, buffer, 1);
+        read(fd, &buffer, 1);
+        write(STDOUT_FILENO, &buffer, 1);
         file_size = lseek(fd, -2, SEEK_CUR);
     }
     write(STDOUT_FILENO, "\n", 1);
