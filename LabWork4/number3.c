72d78ff3dad03625a654903381bc85c8efc962f0
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-03 11:30:57 +0300

    Add a solution to task #3

---
+++ LabWork4/number3.c
@@ -0,0 +1,29 @@
+#include "main_header.h"
+
+const char *my_var = NULL;
+extern char** environ;
+
+int main (int argc, char *argv[]) {
+    my_var = argv[1];
+    int count = 0;
+
+    if (strcmp(my_var, "SOMEVAR") == 0) {
+        while (environ[count] != NULL) {
+            printf("%s\n", environ[count]);
+            count++;
+        }
+    }
+    if (strcmp(my_var, "SOMEVAR=VAR") == 0) {
+        if (setenv("SOMEVAR", "VAR", 0) == -1) {
+            perror("Setenv error: ");
+            return 1;
+        }
+        else {
+            while (environ[count] != NULL) {
+                printf("%s\n", environ[count]);
+                count++;
+            }
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
