ef0f2091a3179d369bde062f06a1e8ceb65ec837
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-04 13:42:59 +0300

    Add a new functional

--- LabWork4/number1.c
+++ LabWork4/number1.c
@@ -1,9 +1,10 @@
 #include "main_header.h"
 
 int main(void) {
-    int pid_t = getpid();
+    int number, pid_t = getpid();
     int ppid_t = getppid();
     printf("PID is: %d\n", pid_t);
     printf("Parrent PID is: %d\n", ppid_t);
+    scanf("%d", &number);
     exit(EXIT_SUCCESS);
 }
