274a8344f6cb56181ab912b0daad72b502f23ef5
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-17 13:23:18 +0300

    Update logic

--- LabWork5/number1.c
+++ LabWork5/number1.c
@@ -2,12 +2,12 @@
 
 int main(int argc, char* argv[]) {
     pid_t pid = fork();
-    
+    printf("\n");
     if (pid == 0) {
         printf("This is child. PID: %d; Parent's PID: %d\n", getpid(), getppid());
     }
     else {
         printf("This is parent. PID: %d; Parent's PID: %d\n", getpid(), pid);
     }
-    return 0;
+    exit(EXIT_SUCCESS);
 }
