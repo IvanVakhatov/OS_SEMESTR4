6c5897996bf4bb3227336d70ef228c72cde076c2
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-17 13:32:46 +0300

    Added a solution to №2

---
+++ LabWork5/number2.c
@@ -0,0 +1,12 @@
+#include "main_header.h"
+
+int main(int argc, char* argv[]) {
+    fork();
+    fork();
+    fork();
+    fork();
+    fork();
+
+    printf("Hello world!\n");
+    exit(EXIT_SUCCESS);
+}
