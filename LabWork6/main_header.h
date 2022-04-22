80ad33b45af1fe75ffc3291261e38d4ef39d31f3
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-22 23:03:14 +0300

    Initial commit

---
+++ LabWork6/main_header.h
@@ -0,0 +1,17 @@
+#ifndef HDR_H
+#define HDR_H /* Предотвращает случайное двойное включение */
+#include <sys/types.h> /* Определения типов, используемые многими
+программами */
+#include <stdio.h> /* Стандартные функции ввода-вывода */
+#include <stdlib.h> /* Прототипы наиболее востребованных библиотечных
+функций плюс константы EXIT_SUCCESS и EXIT_FAILURE */
+#include <unistd.h> /* Прототипы многих системных вызовов */
+#include <fcntl.h> /* Управление файлами и работа
+с файловыми дескрипторами */
+#include <errno.h> /* Объявление errno и определение констант ошибок */
+#include <string.h> /* Наиболее используемые функции обработки строк */
+#include <signal.h>
+#include <pthread.h>
+#include <sys/wait.h>
+typedef enum { FALSE, TRUE } Boolean;
+#endif
