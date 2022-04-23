8388b2da5caf7ca52d1c90b49e39cdfb7a0fad1b
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-04-23 11:49:40 +0300

    Example of pthread usage

---
+++ LabWork6/pthread_ex.c
@@ -0,0 +1,25 @@
+#include "main_header.h"
+#include <pthread.h>
+
+void* process_command_thread(void* p) {
+    int cycles = 10;
+    printf("Hello from process command thread\n");
+    for (int i = 0; i < cycles; i++) {
+        printf("Process command thread cycle [%d]\n", i);
+    }
+    return 0;
+}
+
+int main() {
+    pthread_t h_process_command_thread;
+    int ret;
+
+    ret = pthread_create(&h_process_command_thread, NULL, process_command_thread, NULL);
+    if (ret != 0) {
+        printf("ERROR: pthread_create failed [%d]\n", ret);
+        return -1;
+    }
+
+    printf("Hello from main thread\n");
+    return 0;
+}
