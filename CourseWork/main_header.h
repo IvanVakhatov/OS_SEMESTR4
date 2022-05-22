fda22b06a788b03187d627f87c404ebcd7bcb059
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-05-22 22:33:53 +0300

    Updated build rules

--- CourseWork/main_header.h
+++ CourseWork/main_header.h
@@ -1,12 +1,21 @@
 #ifndef HDR_H
 #define HDR_H /* Предотвращает случайное двойное включение */
-#define CWDSIZE 256
-#define BASHCOMMANDSIZE 33
+
+#define LINUX_PROC_TASK_PATH "/proc"
+#define LINUX_PROC_STAT_PATH "/stat"
 
 #include <stdio.h> /* Стандартные функции ввода-вывода */
 #include <stdlib.h> /* Прототипы наиболее востребованных библиотечных
 функций плюс константы EXIT_SUCCESS и EXIT_FAILURE */
 #include <unistd.h> /* Прототипы многих системных вызовов */
 #include <string.h> /* Наиболее используемые функции обработки строк */
+#include <linux/limits.h>
+#include <sys/eventfd.h>
+#include <sys/poll.h>
+#include <pthread.h>
+#include <poll.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #endif
