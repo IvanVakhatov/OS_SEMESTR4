99b05d42aff1d9d8b0c3437323f5e4e2d12ec66f
Ivan Vakhatov <ivan.vahatov@yandex.ru>
2022-05-23 23:16:34 +0300

    Fixed final bugs

--- CourseWork/generate_stat_csv.c
+++ CourseWork/generate_stat_csv.c
@@ -21,6 +21,34 @@ void csv_preparation(char csv_name[]) {
     return;
 }
 
+void get_cpu_stat(int *total_jiffies, int *work_jiffies) {
+    FILE *proc_stat_file;
+    char buffer[128];
+    int count = 0;
+    int user, nice, system, idle, iowait, irq, softirq;
+
+    proc_stat_file = fopen("/proc/stat", "r");
+    if (proc_stat_file == NULL) {
+        // В случае ошибки - досрочный выход из функции
+        return;
+    }
+
+    while ((fscanf(proc_stat_file, "%s", buffer) == 1) && (count < 8) ) {
+        if (count == 1) sscanf(buffer, "%d", &user);
+        if (count == 2) sscanf(buffer, "%d", &nice);
+        if (count == 3) sscanf(buffer, "%d", &system);
+        if (count == 4) sscanf(buffer, "%d", &idle);
+        if (count == 5) sscanf(buffer, "%d", &iowait);
+        if (count == 6) sscanf(buffer, "%d", &irq);
+        if (count == 7) sscanf(buffer, "%d", &softirq);
+        count++;
+    }
+    *total_jiffies = user+nice+system+idle+iowait+irq+softirq;
+    *work_jiffies = user+nice+system;
+
+    fclose(proc_stat_file);
+}
+
 void get_pid_stat(int pid_number, char csv_name[], char cpu_stat[]) {
     // Указатель на csv файл, в который будут записаны данные
     FILE *csv_file;
@@ -92,21 +120,16 @@ void get_pid_stat(int pid_number, char csv_name[], char cpu_stat[]) {
             }
             if (fscanf(uptime_file, "%s", utime) == 1) {;
                 sscanf(utime, "%d", &SYSTEM_UPTIME_SEC);
-                //printf("%d ", SYSTEM_UPTIME_SEC);
             }
             while ((fscanf(pid_file, "%s", buffer) == 1) && (count < 25) ) {
-                //printf("%s ", buffer);
                 if (count == 13) {
                     sscanf(buffer, "%d", &PROCESS_UTIME);
-                    //printf("%d ", PROCESS_UTIME);
                 }
                 if (count == 14) {
                     sscanf(buffer, "%d", &PROCESS_STIME);
-                    //printf("%d ", PROCESS_STIME);
                 }
                 if (count == 21) {
                     sscanf(buffer, "%d", &PROCESS_STARTTIME);
-                    //printf("%d ", PROCESS_STARTTIME);
                 }
                 count++;
             }
@@ -122,10 +145,7 @@ void get_pid_stat(int pid_number, char csv_name[], char cpu_stat[]) {
                 PROCESS_USAGE_SEC = PROCESS_UTIME_SEC + PROCESS_STIME_SEC;
                 PROCESS_USAGE = PROCESS_USAGE_SEC * 100 / PROCESS_ELAPSED_SEC;
             }
-
-            printf("PID процесса: %6d. Он провел: %4d секунд(ы) в пользовательском режиме", pid_number, PROCESS_UTIME_SEC);
-            printf(", %4d секунд(ы) в режиме ядра. Общая загрузка CPU: %6d секунд(ы). ", PROCESS_STIME_SEC, PROCESS_USAGE_SEC);
-            printf("Процесс запущен уже в течение: %6d секунд(ы). Использование CPU: %4d%c\n", PROCESS_ELAPSED_SEC, PROCESS_USAGE, 37);
+            printf("%8d %10d %8d %10d %7d%c\n", pid_number, PROCESS_UTIME_SEC,PROCESS_STIME_SEC, PROCESS_ELAPSED_SEC, PROCESS_USAGE, 37);
             fclose(uptime_file);
         }
     }
@@ -134,17 +154,21 @@ void get_pid_stat(int pid_number, char csv_name[], char cpu_stat[]) {
     return;
 }
 
-void write_pids_stat(char csv_name[]) {
+void write_pids_stat(char csv_name[], char cpu_stat[], int print_count) {
     DIR * dir_stream = NULL;
 	struct dirent * procfs_entry = NULL;
 	dir_stream = opendir(LINUX_PROC_TASK_PATH);
 	pid_t pid;
+    int count = 0;
 
 	if(dir_stream == NULL) {
 		perror("opendir");
 	}
 
 	while((procfs_entry = readdir(dir_stream)) != NULL) {
+        if (print_count == -1 && count == 23) {
+            break;
+        }
 		char *sym;
 		bool acceptable_name = true;
 		for(sym = procfs_entry->d_name; *sym; sym++) {
@@ -161,16 +185,39 @@ void write_pids_stat(char csv_name[]) {
 		if(pid <= 0) {
 			continue;
 		}
-        get_pid_stat(pid, csv_name, "yes");
-        //get_pid_stat(pid, csv_name, "no");
+        get_pid_stat(pid, csv_name, cpu_stat);
+        count++;
+
 	}
 	(void)closedir(dir_stream);
     return;
 }
 
+void print_cpu_stat(int seconds) {
+    int total_jiffies_1, work_jiffies_1;
+    int total_jiffies_2, work_jiffies_2;
+    int work_over_period, total_over_period;
+    double cpu_util;
+
+    get_cpu_stat(&total_jiffies_1, &work_jiffies_1);
+    sleep(seconds);
+    get_cpu_stat(&total_jiffies_2, &work_jiffies_2);
+
+    work_over_period = work_jiffies_2 - work_jiffies_1;
+    total_over_period = total_jiffies_2 - total_jiffies_1;
+    cpu_util = work_over_period / total_over_period * 100;
+
+    write_pids_stat("proc_stat.csv", "yes", 0);
+    printf("%8s %10s %8s %10s %8s\n", "PID", "USER_SEC", "KERN_SEC", "CPU_TIME", "%CPU");
+    printf("\nCPU UTILIZATION: %5lf%c\n", cpu_util, 37);
+}
+
 int main(int argc, char *argv[]) {
     csv_preparation("proc_stat.csv");
-    write_pids_stat("proc_stat.csv");
+    write_pids_stat("proc_stat.csv", "no", 0);
     printf("Генерирование csv файла со статистическими данными завершено!\n");
+    while (42) {
+        print_cpu_stat(1);
+    }
     return 0;
 }
